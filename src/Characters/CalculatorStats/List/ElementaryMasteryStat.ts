7f16d99b5bb1001a4f067165d772616761af3119
TheEZIC <sasha4488s@gmail.com>
2021-10-25 01:53:21 +0300

    Add reactions DMG bonus

--- src/Characters/CalculatorStats/List/ElementaryMasteryStat.ts
+++ src/Characters/CalculatorStats/List/ElementaryMasteryStat.ts
@@ -8,4 +8,19 @@ export default class ElementaryMasteryStat extends PureStat {
 
     return elementalMastery.value + artifactsElementalMastery + this.additionalValuesSum;
   }
+
+  public get vaporizeAndMeltReactionBonus() {
+    const EM = this.calc();
+    return 278 * (EM / (EM + 1400));
+  }
+
+  public get anemoAndElectroReactionBonus() {
+    const EM = this.calc();
+    return 1600 * (EM / (EM + 2000));
+  }
+
+  public get crystallizeReactoinBonus() {
+    const EM = this.calc();
+    return 444 * (EM / (EM + 1400));
+  }
 }
