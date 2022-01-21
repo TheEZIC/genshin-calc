488581053cc3fb7dc0cf4aaf5318f5cdb0625649
TheEZIC <sasha4488s@gmail.com>
2022-01-21 11:09:28 +0300

    Update ConstellationsManager.ts

--- src/Constellations/ConstellationsManager.ts
+++ src/Constellations/ConstellationsManager.ts
@@ -8,19 +8,40 @@ export default class ConstellationsManager {
   ) {}
 
   public activateConstellation(order: number) {
+    this.deactivateAll();
+
     const index = order - 1;
 
     for (let i = 0; i < this.constellations.length; i++) {
-      if (i < index) {
-
+      if (i <= index) {
+        const constellation = this.constellations[i];
+        constellation.activate(this.character);
       }
-      const constellation = this.constellations[i];
-
     }
-    this.constellations[index].activate(this.character);
   }
 
   public deactivateConstellation(order: number) {
-    this.constellations[order - 1].deactivate(this.character);
+    this.activateAll();
+
+    const index = order - 1;
+
+    for (let i = 0; i < this.constellations.length; i++) {
+      if (i >= index) {
+        const constellation = this.constellations[i];
+        constellation.deactivate(this.character);
+      }
+    }
+  }
+
+  public activateAll() {
+    for (let constellation of this.constellations) {
+      constellation.activate(this.character);
+    }
+  }
+
+  public deactivateAll() {
+    for (let constellation of this.constellations) {
+      constellation.deactivate(this.character);
+    }
   }
 }
